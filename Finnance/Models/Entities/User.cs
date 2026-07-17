4db570e4b3a36cf97c092308147da208551e3320
Luiz Bortolanza <luizbortolanza0@gmail.com>
2026-07-17 17:52:57 -0300

    Add User entity with properties and Email value object for validation

--- Finnance/Models/Entities/User.cs
+++ Finnance/Models/Entities/User.cs
@@ -1,6 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using Finnance.Domain.ValueObjects;
+
 namespace Finnance.Models.Entities;
+
+/*
+User
+- Id (Guid)
+- Name (Varchar (80))
+- Email (Varchar (80))
+- PasswordHash (Varchar(255))
+- CreatedAt DateTime
+- UpdatedAt DateTime
+- DeletedAt DateTime
+- IsActive Boolean
+*/
 
 public class User
 {
+  public Guid Id { get; private set; }
+  public string UserName { get; private set; }
+  public Email Email { get; private set; }
+  public string PasswordHash { get; private set; }
+  public DateTime CreatedAt { get; private set; }
+  public DateTime? UpdatedAt { get; private set; }
+  public DateTime? DeletedAt { get; private set; }
+  public bool IsActive { get; private set; }
+
+  // Necessario no EF core!
+  private User()
+  {
+  }
+  private User(string username, Email email, string password)
+  {
+    Id = Guid.NewGuid();
+    UserName = username;
+    Email = email;
+    PasswordHash = password;
+    CreatedAt = DateTime.UtcNow;
+    IsActive = true;
+  }
+
+  static public User Create(string userName, string email, string password)
+  {
+    if (string.IsNullOrWhiteSpace(userName))
+      throw new ArgumentException("O username é obrigatorio!");
+
+    if (string.IsNullOrWhiteSpace(password))
+      throw new ArgumentException("Senha é obrigatoria!");
+
+    var emailValido = new Email(email);
+
+    return new User(userName, emailValido, password);
+  }
 
 }
---
+++ Finnance/Models/ValueObjects/Email.cs
@@ -0,0 +1,31 @@
+using System.Text.RegularExpressions;
+
+namespace Finnance.Domain.ValueObjects;
+
+public sealed class Email
+{
+  // Necessario no EF core!
+  private Email()
+  {
+  }
+  private static readonly Regex EmailRegex = new(
+      @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+      RegexOptions.Compiled);
+
+  public string Value { get; private set; }
+
+  public Email(string value)
+  {
+    if (string.IsNullOrWhiteSpace(value))
+      throw new ArgumentException("E-mail é obrigatório.");
+
+    if (!EmailRegex.IsMatch(value))
+      throw new ArgumentException("E-mail inválido.");
+
+    Value = value;
+  }
+
+  public override string ToString() => Value;
+
+  public static implicit operator string(Email email) => email.Value;
+}
